09a0d695927a3447b619c0a359d68f6f00fbc092
Cyril Battistolo <cybattis@student.42lyon.fr>
2022-04-14 13:22:28 +0200

    redo print macro cleaning and add commentary

--- utest/utest.h
+++ utest/utest.h
@@ -6,7 +6,7 @@
 /*   By: cybattis <cybattis@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 11:28:17 by cybattis          #+#    #+#             */
-/*   Updated: 2022/04/13 17:42:09 by cybattis         ###   ########.fr       */
+/*   Updated: 2022/04/14 13:21:03 by cybattis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,11 @@
 /*     Utils    */
 /* ************ */
 
+/*  Set a test run for function who print to stdout */
+#define UT_PRINT_TEST	-2
+/*  Ignore a test */
 #define UT_IGNORE		-1
+
 #define UT_FAIL			0
 #define UT_PASS			1
 #define UT_SIG			2
@@ -46,63 +50,83 @@
 #define _GREY			"\x1b[30m"
 #define _RESET			"\x1b[0m"
 
-#define UT_ERROR		"[\033[31mERROR\033[0m]"
-#define UT_WARNING		"[\033[33mWARNING\033[0m]"
-#define UT_INFO			"[\033[32mINFO\033[0m]"
-#define UT_DEBUG		"[\033[34mDEBUG\033[0m]"
-#define UT_FATAL		"[\033[31mFATAL\033[0m]"
-
-#define PRINTF_FORMAT(x) _Generic((x), \
-    char: "%c", \
-    signed char: "%hhd", \
-    unsigned char: "%hhu", \
-    signed short: "%hd", \
-    unsigned short: "%hu", \
-    signed int: "%d", \
-    unsigned int: "%u", \
-    long int: "%ld", \
-    unsigned long int: "%lu", \
-    long long int: "%lld", \
-    unsigned long long int: "%llu", \
-    float: "%f", \
-    double: "%f", \
-    long double: "%Lf", \
-    char *: "%s", \
-    void *: "%p")
-
-#define PRINT_MSG(actual, msg, expected) \
-		dprintf(STDERR_FILENO, PRINTF_FORMAT(actual), actual),		\
-		dprintf(STDERR_FILENO, msg), 			\
-        dprintf(STDERR_FILENO, PRINTF_FORMAT(expected), expected),	\
-		dprintf(STDERR_FILENO, "\n")
+#define UT_ERROR		"["_RED"ERROR"_RESET"]"
+#define UT_WARNING		"["_YELLOW"WARNING"_RESET"]"
+#define UT_INFO			"["_GREEN"INFO"_RESET"]"
+#define UT_DEBUG		"["_BLUE"DEBUG"_RESET"]"
+#define UT_FATAL		"["_RED"FATAL"_RESET"]"
 
 #define VAR_NAME(var)	#var
+
+#define PRINTF_FORMAT(x) _Generic((x), \
+	char: "%c", \
+	signed char: "%hhd", \
+	unsigned char: "%hhu", \
+	signed short: "%hd", \
+	unsigned short: "%hu", \
+	signed int: "%d", \
+	unsigned int: "%u", \
+	long int: "%ld", \
+	unsigned long int: "%lu", \
+	long long int: "%lld", \
+	unsigned long long int: "%llu", \
+	float: "%f", \
+	double: "%f", \
+	long double: "%Lf", \
+	char *: "%s", \
+	void *: "%p", \
+	default: "%zu")
+
 
 /* Macros print */
 /* ************ */
 
-#define UTEST_PRINT_ASSERT_STR(level, actual, expected)			\
-		dprintf(UTEST_OUT, "%s in "_YELLOW"%s\033[0m() /%s:%d: Expected: \"%s\" --> actual: \"%s\"\n", \
-		level, __FUNCTION__ , __FILE__, __LINE__, expected, actual)
-
-#define UTEST_PRINT_ASSERT_INT(level)	\
+/* [LEVEL] in function() /path/to/file:line: */
+#define PRINT_STAMP(level) \
 		dprintf(UTEST_OUT, "%s in "_YELLOW"%s\033[0m() /%s:%d: ", level, __FUNCTION__ , __FILE__, __LINE__)
 
-#define UTEST_PRINT_ASSERT_BOOL(level, msg)						\
-		dprintf(UTEST_OUT, "%s in "_YELLOW"%s\033[0m() /%s:%d: "msg"\n", \
-		level, __FUNCTION__ , __FILE__, __LINE__)
+#define PRINT_NL \
+		dprintf(STDERR_FILENO, "\n")
 
-#define UTEST_PRINT_ASSERT_PTR(level, var, expected) \
-		dprintf(UTEST_OUT, "%s in "_YELLOW"%s\033[0m() /%s:%d: Pointer %s %s\n", \
-		level, __FUNCTION__ , __FILE__, __LINE__, VAR_NAME(var), expected)
+#define PRINT_MSG_ASSERT_TYPE(actual, msg, expected)				\
+		dprintf(STDERR_FILENO, PRINTF_FORMAT(actual), actual),		\
+		dprintf(STDERR_FILENO, PRINTF_FORMAT(msg), msg), 			\
+		dprintf(STDERR_FILENO, PRINTF_FORMAT(expected), expected),	\
+		dprintf(STDERR_FILENO, "\n")
+
+/* [LEVEL] in function() /path/to/file:line: "actual msg expected" */
+#define UTEST_PRINT_ASSERT_ERROR_INT(level, actual, msg, expected)	\
+		PRINT_STAMP(level),											\
+		PRINT_MSG_ASSERT_TYPE(actual, msg, expected)
+
+/* [LEVEL] in function() /path/to/file:line: */
+#define UTEST_PRINT_ASSERT_ERROR(level, ...)	\
+		PRINT_STAMP(level),											\
+		dprintf(STDERR_FILENO, __VA_ARGS__),						\
+		dprintf(STDERR_FILENO, "\n")
 
 
 /* *************************** */
 /*             Suite           */
 /* *************************** */
 
+/*
+ * Start and initialize a test suite and give it a name
+ */
 #define UTEST_BEGIN(suite_name)							utest_begin(suite_name, UTEST_OUT)
+
+/*
+ * Usage: RUN_TEST("test_name", function, option)
+ *
+ * Option:
+ *  - UT_PRINT_TEST : Setup a test run for function who print to stdout
+ *  - UT_IGNORE : Ignore a test
+ */
 #define RUN_TEST(test_name, func, ...)					run_test(test_name, func, ##__VA_ARGS__)
+
+/*
+ * Print stats of the test suit
+ */
 #define UTEST_END()										utest_end(UTEST_OUT)
 
 
@@ -110,61 +134,61 @@
 /*            Assert           */
 /* *************************** */
 
+/* Assert String */
+
 #define UTEST_ASSERT_STR_EQUAL(actual, expected)		\
 		if (strcmp(actual, expected))					\
-			UTEST_PRINT_ASSERT_STR(UT_ERROR, actual, expected)
+			UTEST_PRINT_ASSERT_ERROR(UT_ERROR, "Expected \"%s\" --> Actual \"%s\"", actual, expected)
 
 #define UTEST_ASSERT_STR_NOT_EQUAL(actual, expected)	\
 		if (!strcmp(actual, expected))					\
-			UTEST_PRINT_ASSERT_STR(UT_ERROR, actual, expected)
+			UTEST_PRINT_ASSERT_ERROR(UT_ERROR, "Expected \"%s\" --> Actual \"%s\"", actual, expected)
 
+/* Assert Bool */
 
 #define UTEST_ASSERT_TRUE(value)		\
 		if (value)						\
-			UTEST_PRINT_ASSERT_BOOL(UT_ERROR, "Expected TRUE is FALSE")
+			UTEST_PRINT_ASSERT_ERROR(UT_ERROR, "Expected TRUE is FALSE")
 
 #define UTEST_ASSERT_FALSE(value)		\
 		if (!value)						\
-			UTEST_PRINT_ASSERT_BOOL(UT_ERROR, "Expected FALSE is TRUE")
+			UTEST_PRINT_ASSERT_ERROR(UT_ERROR, "Expected FALSE is TRUE")
 
+/* Assert all type of integer */
 
 #define UTEST_ASSERT_INT_EQUAL(actual, expected)			\
 		if (actual != expected)								\
-			UTEST_PRINT_ASSERT_INT(UT_ERROR),				\
-			PRINT_MSG(actual, " is not equal to ", expected)
+			UTEST_PRINT_ASSERT_ERROR_INT(UT_ERROR, actual, " is not equal to ", expected)
 
 #define UTEST_ASSERT_INT_NOT_EQUAL(actual, expected)		\
 		if (actual == expected)								\
-			UTEST_PRINT_ASSERT_INT(UT_ERROR),				\
-			PRINT_MSG(actual, " is equal to ", expected)
+			UTEST_PRINT_ASSERT_ERROR_INT(UT_ERROR, actual, " is equal to ", expected)
 
 #define UTEST_ASSERT_INT_GREATER(actual, expected)			\
 		if (actual <= expected)								\
-			UTEST_PRINT_ASSERT_INT(UT_ERROR),				\
-			PRINT_MSG(actual, " is not greater than ", expected)
+			UTEST_PRINT_ASSERT_ERROR_INT(UT_ERROR, actual, " is not greater than ", expected)
 
 #define UTEST_ASSERT_INT_GREATER_EQUAL(actual, expected)	\
 		if (actual < expected)								\
-			UTEST_PRINT_ASSERT_INT(UT_ERROR),				\
-			PRINT_MSG(actual, " is not greater or equal to ", expected)
+			UTEST_PRINT_ASSERT_ERROR_INT(UT_ERROR, actual, " is not greater or equal to ", expected)
 
 #define UTEST_ASSERT_INT_LESSER(actual, expected)			\
 		if (actual >= expected)								\
-			UTEST_PRINT_ASSERT_INT(UT_ERROR),				\
-			PRINT_MSG(actual, " is greater to ", expected)
+			UTEST_PRINT_ASSERT_ERROR_INT(UT_ERROR, actual, " is greater to ", expected)
 
 #define UTEST_ASSERT_INT_LESSER_EQUAL(actual, expected)		\
 		if (actual > expected)								\
-			UTEST_PRINT_ASSERT_INT(UT_ERROR),				\
-			PRINT_MSG(actual, " is greater or equal to ", expected)
+			UTEST_PRINT_ASSERT_ERROR_INT(UT_ERROR, actual, " is greater or equal to ", expected)
+
+/* Assert pointer */
 
 #define UTEST_ASSERT_PTR_NULL(actual)		\
 		if (actual)							\
-			UTEST_PRINT_ASSERT_PTR(UT_ERROR, actual, "has to be null")
+			UTEST_PRINT_ASSERT_ERROR(UT_ERROR, VAR_NAME(actual) " is not null")
 
 #define UTEST_ASSERT_PTR_NOT_NULL(actual)	\
 		if (!actual)						\
-			UTEST_PRINT_ASSERT_PTR(UT_ERROR, actual, "has to be not null")
+			UTEST_PRINT_ASSERT_ERROR(UT_ERROR, VAR_NAME(actual) " is null")
 
 
 /* *************************** */
@@ -200,7 +224,8 @@ static void	utest_end(int fd)
 
 static int	run_test(char *test_name, int (*f)(void), ...)
 {
-	int 	flags;
+	int 	fd;
+	int 	flags[2];
 	va_list args;
 	pid_t	pid;
 	int		return_value;
@@ -208,24 +233,30 @@ static int	run_test(char *test_name, int (*f)(void), ...)
 
 	utest_suite.count++;
 	va_start(args, f);
-	flags = va_arg(args, int);
+	flags[0] = va_arg(args, int);
+	flags[1] = va_arg(args, int);
 	va_end(args);
-	if (flags == UT_IGNORE)
-	{
-
-		print_test_status(UT_IGNORE, test_name, STDERR_FILENO);
-		return (-1);
+	for (int i = 0; i < 2; ++i) {
+		if (flags[i] == UT_IGNORE) {
+			print_test_status(UT_IGNORE, test_name, STDERR_FILENO);
+			return (-1);
+		}
 	}
 	pid = fork();
 	if (pid == -1)
 		return (-1);
 	else if (!pid)
 	{
-		int	fd = open("tmp", O_WRONLY | O_CREAT, 0644);
-		if (fd == -1)
-			exit(-1);
-		dup2(fd, 1);
-		dup2(fd, 2);
+		fd = 0;
+		for (int i = 0; i < 2; ++i) {
+			if (flags[i] == UT_PRINT_TEST) {
+				if ((fd = open("tmp", O_WRONLY | O_CREAT, 0644)) == -1)
+					exit(-1);
+				dup2(fd, 1);
+				dup2(fd, 2);
+				break;
+			}
+		}
 		alarm(UT_TIMEOUT);
 		return_value = f();
 		close(fd);
